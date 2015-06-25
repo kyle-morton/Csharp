67a94287972eabf8f177165f34753a448cd08905
kylemorton5770 <kylemortondevelopment@gmail.com>
2015-06-25 16:13:33 -0500

    first commit

---
+++ BeginnerApp/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Person
+    {
+
+        //Backing Private Field
+        private String name;
+
+        //Public Property (Mutators)
+        public String personName { get; set; }
+
+    }
+}
---
+++ BeginnerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace BeginnerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.personName = "Ted";
+            Console.WriteLine("Name: {0}", p.personName);
+            Console.ReadKey();
+        }
+    }
+}
